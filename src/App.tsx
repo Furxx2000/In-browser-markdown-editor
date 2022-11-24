baac5368c6f3fd4f2b633f1ce72b338ba8becd00
furxx2000 <furxx2000@gmail.com>
2022-11-24 23:56:04 +0800

    Import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import ModeSwitcher from './components/ModeSwitcher';
+import NewDocumentBtn from './components/NewDocumentBtn';
+import SaveChangeBtn from './components/SaveChangeBtn';
 import './App.scss';
 
 function App() {
   return (
-    <div>
-      <div>Hello world!</div>
+    <div className='flex'>
+      <SaveChangeBtn />
+      <NewDocumentBtn />
+      <ModeSwitcher />
     </div>
   );
 }
