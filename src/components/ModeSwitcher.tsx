9fd586fa7e39e4664e216311c74cf75bfe2df95e
furxx2000 <furxx2000@gmail.com>
2022-11-25 00:09:13 +0800

    Complete mode toggle function

--- src/components/ModeSwitcher.tsx
+++ src/components/ModeSwitcher.tsx
@@ -4,6 +4,7 @@ import '../scss/ModeSwitcher.scss';
 
 function ModeSwitcher() {
   const [isDarkMode, setMode] = useState(false);
+  const notActive = '#5a6069';
 
   function handleToggleMode() {
     setMode((current) => !current);
@@ -12,9 +13,15 @@ function ModeSwitcher() {
 
   return (
     <div className='mode-switcher bg-dark-3 flex'>
-      <SvgIcon name='icon-dark-mode' color='white' />
-      <div className='toggler bg-gray-1' onClick={handleToggleMode}></div>
-      <SvgIcon name='icon-light-mode' color='white' />
+      <SvgIcon name='icon-dark-mode' color={isDarkMode ? 'white' : notActive} />
+      <div
+        className={`toggler bg-gray-1 ${isDarkMode ? 'dark-mode' : ''}`}
+        onClick={handleToggleMode}
+      ></div>
+      <SvgIcon
+        name='icon-light-mode'
+        color={isDarkMode ? notActive : 'white'}
+      />
     </div>
   );
 }
