6c8a6af28820c8145d8aa6fe1fd1e868323b954f
Danny Wen <danny_wen@trendmicro.com>
2022-11-29 17:51:22 +0800

    Create component for switch markdown and preview

---
+++ src/components/UI/GrayHeader.tsx
@@ -0,0 +1,20 @@
+import '../../scss/Markdown.scss';
+import SvgIcon from './SvgIcon';
+
+interface Props {
+  text: string;
+  icon: string;
+  onChangeMarkdownStatus: () => void;
+}
+
+function GrayHeader({ text, icon, onChangeMarkdownStatus }: Props) {
+  const grayColor = '#7C8187';
+  return (
+    <div className='markdown-header fs-250 fw-medium letter-spacing-1 text-gray-2 bg-white-1 flex'>
+      {text}
+      <SvgIcon name={icon} color={grayColor} onClick={onChangeMarkdownStatus} />
+    </div>
+  );
+}
+
+export default GrayHeader;
