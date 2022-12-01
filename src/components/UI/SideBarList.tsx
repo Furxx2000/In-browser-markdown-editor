322aa1eb5ffb2c3e893bb39e0453d4e7493be03b
Danny Wen <danny_wen@trendmicro.com>
2022-12-01 18:37:36 +0800

    Create new component to display the file list in menu

---
+++ src/components/UI/SideBarList.tsx
@@ -0,0 +1,24 @@
+import '../../scss/SideBarList.scss';
+import SvgIcon from './SvgIcon';
+
+interface Props {
+  fileName: string;
+  date: string;
+  isSelected: boolean;
+}
+
+function SideBarList({ fileName, date, isSelected }: Props) {
+  return (
+    <div className='side-bar-list flex text-white'>
+      <SvgIcon name='icon-document' color='white' />
+      <div className='document-name'>
+        <span className='fs-200 fw-light text-gray-3'>{date}</span>
+        <p className={`${isSelected ? 'text-orange-1' : 'text-white'}`}>
+          {fileName}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SideBarList;
