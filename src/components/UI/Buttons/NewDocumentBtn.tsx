bc16ad2cac84f9d54bad8f3302acd8faa109fbe6
Danny Wen <danny_wen@trendmicro.com>
2022-12-01 18:37:48 +0800

    Add handler to add new document

--- src/components/UI/Buttons/NewDocumentBtn.tsx
+++ src/components/UI/Buttons/NewDocumentBtn.tsx
@@ -1,10 +1,15 @@
 import '../../../scss/Button.scss';
 
-function NewDocumentBtn() {
+interface Props {
+  addNewDocument: () => void;
+}
+
+function NewDocumentBtn({ addNewDocument }: Props) {
   return (
     <button
       type='button'
       className='new-document-btn bg-orange-1 rounded text-white'
+      onClick={addNewDocument}
     >
       + New Document
     </button>
