0651c28c02a18096f132174aabdeb8e2824413e4
Danny Wen <danny_wen@trendmicro.com>
2022-12-01 18:36:44 +0800

    Add handlers and use transition

--- src/components/UI/Dialog.tsx
+++ src/components/UI/Dialog.tsx
@@ -2,17 +2,31 @@ import '../../scss/Dialog.scss';
 
 interface Props {
   name: string;
+  timeStamp: string;
+  isOpenDialog: boolean;
   changeDialogStatus: () => void;
+  deleteCurFile: (fileName: string) => void;
 }
 
-function Dialog({ changeDialogStatus, name }: Props) {
+function Dialog({
+  changeDialogStatus,
+  deleteCurFile,
+  name,
+  timeStamp,
+  isOpenDialog,
+}: Props) {
   return (
     <>
-      <div className='backdrop bg-dark-1' onClick={changeDialogStatus}></div>
+      <div
+        className={`backdrop bg-dark-1 ${isOpenDialog ? 'is-active' : ''}`}
+        onClick={changeDialogStatus}
+      ></div>
       <div
         role='dialog'
         aria-label='Delete this document?'
-        className='dialog bg-white-1 rounded flex'
+        className={`dialog bg-white-1 rounded flex ${
+          isOpenDialog ? 'is-active' : ''
+        }`}
       >
         <h4 className='dialog-title'>Delete this document?</h4>
         <p className='dialog-description ff-roboto-slab fs-250 text-gray-2'>
@@ -22,6 +36,7 @@ function Dialog({ changeDialogStatus, name }: Props) {
         <button
           type='button'
           className='dialog-btn bg-orange-1 text-white rounded'
+          onClick={() => deleteCurFile(timeStamp)}
         >
           Confirm & Delete
         </button>
