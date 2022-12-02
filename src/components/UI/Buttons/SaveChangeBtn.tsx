ea87231e4d87c430513db911b3dd31c72a9a2636
Danny Wen <danny_wen@trendmicro.com>
2022-12-02 19:09:43 +0800

    Add saveChange handler

--- src/components/UI/Buttons/SaveChangeBtn.tsx
+++ src/components/UI/Buttons/SaveChangeBtn.tsx
@@ -1,13 +1,18 @@
 import SvgIcon from '../SvgIcon';
 import '../../../scss/Button.scss';
 
-function SaveChangeBtn() {
+interface Props {
+  saveChangedName: () => void;
+}
+
+function SaveChangeBtn({ saveChangedName }: Props) {
   const mql = window.matchMedia('(max-width: 480px)');
 
   return (
     <button
       type='button'
       className='save-change bg-orange-1 rounded text-white'
+      onClick={saveChangedName}
     >
       <SvgIcon name='icon-save' color='white' />
       {mql.matches ? '' : <p>Save Change</p>}
