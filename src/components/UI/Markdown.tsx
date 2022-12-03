5969aa71f8a36a0b9d3ebb41dd5e629fdd6b239f
furxx2000 <furxx2000@gmail.com>
2022-12-03 21:17:43 +0800

    Remove handler then use pre element for markdown

--- src/components/UI/Markdown.tsx
+++ src/components/UI/Markdown.tsx
@@ -1,20 +1,13 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import '../../scss/Markdown.scss';
 import GrayHeader from './GrayHeader';
 
 interface Props {
   content: string;
-  markdownVal: string;
   onChangeMarkdownStatus: () => void;
-  changeMarkdownVal: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-function Markdown({
-  content,
-  markdownVal,
-  changeMarkdownVal,
-  onChangeMarkdownStatus,
-}: Props) {
+function Markdown({ content, onChangeMarkdownStatus }: Props) {
   useEffect(() => {}, [content]);
 
   return (
@@ -24,13 +17,9 @@ function Markdown({
         icon='icon-show-preview'
         onChangeMarkdownStatus={onChangeMarkdownStatus}
       />
-      <textarea
-        id='markdown'
-        className='markdown-content ff-roboto-mono fs-250 text-dark-4'
-        name='markdown'
-        value={markdownVal}
-        onChange={changeMarkdownVal}
-      ></textarea>
+      <pre className='markdown-content ff-roboto-mono fs-250 text-dark-4'>
+        {content}
+      </pre>
     </section>
   );
 }
