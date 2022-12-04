86efa631f8eba34b9cb734a41d369d629ca29157
furxx2000 <furxx2000@gmail.com>
2022-12-04 23:57:20 +0800

    Add dark mode color settings

--- src/components/UI/Preview.tsx
+++ src/components/UI/Preview.tsx
@@ -3,18 +3,24 @@ import GrayHeader from './GrayHeader';
 
 interface Props {
   content: string;
+  isDarkMode: boolean;
   onChangeMarkdownStatus: () => void;
 }
 
-function Preview({ content, onChangeMarkdownStatus }: Props) {
+function Preview({ content, isDarkMode, onChangeMarkdownStatus }: Props) {
   return (
     <section className='preview'>
       <GrayHeader
         icon='icon-hide-preview'
         text='PREVIEW'
+        isDarkMode={isDarkMode}
         onChangeMarkdownStatus={onChangeMarkdownStatus}
       />
-      <pre className='preview-content ff-roboto-mono fs-250 text-dark-4'>
+      <pre
+        className={`markdown-content ff-roboto-mono fs-250 text-${
+          isDarkMode ? 'gray-3' : 'dark-4'
+        } ${isDarkMode ? 'bg-dark-1' : ''}`}
+      >
         {content}
       </pre>
     </section>
