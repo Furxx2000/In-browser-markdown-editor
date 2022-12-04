7241e0c8ec520758570bb2ca8e0a8d3e4ac8614a
furxx2000 <furxx2000@gmail.com>
2022-12-04 23:56:40 +0800

    Add dark mode color settings

--- src/components/UI/GrayHeader.tsx
+++ src/components/UI/GrayHeader.tsx
@@ -4,13 +4,19 @@ import SvgIcon from './SvgIcon';
 interface Props {
   text: string;
   icon: string;
+  isDarkMode: boolean;
   onChangeMarkdownStatus: () => void;
 }
 
-function GrayHeader({ text, icon, onChangeMarkdownStatus }: Props) {
-  const grayColor = '#7C8187';
+function GrayHeader({ text, icon, isDarkMode, onChangeMarkdownStatus }: Props) {
+  const grayColor = isDarkMode ? '#C1C4CB' : '#7C8187';
+
   return (
-    <div className='markdown-header fs-250 fw-medium letter-spacing-1 text-gray-2 bg-white-1 flex'>
+    <div
+      className={`markdown-header fs-250 fw-medium letter-spacing-1 text-gray-${
+        isDarkMode ? '3' : '2'
+      } ${isDarkMode ? 'bg-dark-2' : 'bg-white-1'} flex`}
+    >
       {text}
       <SvgIcon name={icon} color={grayColor} onClick={onChangeMarkdownStatus} />
     </div>
