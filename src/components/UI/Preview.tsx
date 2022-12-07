fd810b59da818c8426c60c4c77bdb3bbe0229230
furxx2000 <furxx2000@gmail.com>
2022-12-08 00:54:14 +0800

    Import help func to convert markdown to preview

--- src/components/UI/Preview.tsx
+++ src/components/UI/Preview.tsx
@@ -1,6 +1,7 @@
 import '../../scss/Preview.scss';
 import GrayHeader from './GrayHeader';
 import { useEffect, useState } from 'react';
+import ConvertMarkdownToPreview from '../../helpers/convertMarkdownToPreview';
 
 interface Props {
   content: string;
@@ -12,68 +13,8 @@ function Preview({ content, isDarkMode, onChangeMarkdownStatus }: Props) {
   const [htmlContent, setHtmlContent] = useState('');
 
   useEffect(() => {
-    const isHeadingText = /^#+\s+/g;
-    const isOrderedList = /^[0-9].\s+/g;
-    const isUnOrderedList = /^-\s+/g;
-    const isBlockQuote = /^>\s+/g;
-    const isHyperLink = /\[\w+\s*\w+\]\(https:\/\/.+\)/g;
-
-    const markdownTemplate = content
-      .split('\n\n')
-      .map((el) => {
-        if (isHeadingText.test(el)) {
-          const regexForHashtag = /#+/g;
-          const tagNumber = el.match(regexForHashtag)![0].length;
-
-          return `<h${tagNumber}>${el
-            .replaceAll('#', '')
-            .trim()}</h${tagNumber}>`;
-        }
-
-        if (isOrderedList.test(el)) {
-          const listArr = el
-            .split('\n')
-            .map((f) => f.replaceAll(isOrderedList, ''));
-          const orderedTemp = `
-          <ol class='flow'>
-            ${listArr.map((el) => `<li>${el}</li>`)}
-          </ol>
-          `.replaceAll(',', '');
-          return orderedTemp;
-        }
-
-        if (isUnOrderedList.test(el)) {
-          const listArr = el
-            .split('\n')
-            .map((f) => f.replaceAll(isUnOrderedList, ''));
-          const unOrderedTemp = `
-            <ul class='flow'>
-              ${listArr.map((a) => `<li>${a}</li>`)}
-            </ul>
-          `.replaceAll(',', '');
-          return unOrderedTemp;
-        }
-
-        if (isBlockQuote.test(el)) {
-          console.log(el.match(isHyperLink));
-          const blockQuoteTemp = `
-            <div class='block-quote'>
-              <p>
-                ${el.replace('>', '')}
-              </p>
-            </div>
-          `;
-          return blockQuoteTemp;
-        }
-
-        if (isHyperLink.test(el)) {
-          console.log('Hi');
-        }
-
-        return `<p>${el}</p>`;
-      })
-      .join('\n');
-    setHtmlContent(markdownTemplate);
+    const temp = ConvertMarkdownToPreview(content);
+    setHtmlContent(temp);
   }, [content]);
 
   return (
