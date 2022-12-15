f5e3c8f9f7eb9bef03ccbc40c4348d06fb326211
Danny Wen <danny_wen@trendmicro.com>
2022-12-15 17:59:16 +0800

    Use uppercase

--- src/components/UI/Preview.tsx
+++ src/components/UI/Preview.tsx
@@ -1,7 +1,7 @@
 import '../../scss/Preview.scss';
 import GrayHeader from './GrayHeader';
 import { useEffect, useState } from 'react';
-import ConvertMarkdownToPreview from '../../helpers/convertMarkdownToPreview';
+import ConvertMarkdownToPreview from '../../helpers/ConvertMarkdownToPreview';
 
 interface Props {
   content: string;
