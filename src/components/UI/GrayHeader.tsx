99fc9bcb5871a62946e00e9ebfc60cd50c651d89
Danny Wen <danny_wen@trendmicro.com>
2022-12-16 18:07:00 +0800

    Add more style to handle tablet and mobile version

--- src/components/UI/GrayHeader.tsx
+++ src/components/UI/GrayHeader.tsx
@@ -5,20 +5,45 @@ interface Props {
   text: string;
   icon: string;
   isDarkMode: boolean;
-  onChangeMarkdownStatus: () => void;
+  onChangeMarkdownStatus?: () => void;
+  changeOnePagePreviewStatus?: () => void;
 }
 
-function GrayHeader({ text, icon, isDarkMode, onChangeMarkdownStatus }: Props) {
+function GrayHeader({
+  text,
+  icon,
+  isDarkMode,
+  onChangeMarkdownStatus,
+  changeOnePagePreviewStatus,
+}: Props) {
   const grayColor = isDarkMode ? '#C1C4CB' : '#7C8187';
+  const mql = window.matchMedia('(max-width: 480px)');
+  const isTabletMarkdown = !mql.matches && text === 'MARKDOWN';
 
   return (
     <div
       className={`markdown-header fs-250 fw-medium letter-spacing-1 text-gray-${
         isDarkMode ? '3' : '2'
-      } ${isDarkMode ? 'bg-dark-2' : 'bg-white-1'} flex`}
+      } ${isDarkMode ? 'bg-dark-2 dark-mode' : 'bg-white-1'} ${
+        isTabletMarkdown ? 'b-right-gray' : ''
+      } flex`}
     >
       {text}
-      <SvgIcon name={icon} color={grayColor} onClick={onChangeMarkdownStatus} />
+      {isTabletMarkdown ? (
+        ''
+      ) : mql.matches ? (
+        <SvgIcon
+          name={icon}
+          color={grayColor}
+          onClick={onChangeMarkdownStatus}
+        />
+      ) : (
+        <SvgIcon
+          name={icon}
+          color={grayColor}
+          onClick={changeOnePagePreviewStatus}
+        />
+      )}
     </div>
   );
 }
