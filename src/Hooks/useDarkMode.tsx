1bfb2c8212ea7728e955ac8e7344a0467617b681
furxx2000 <furxx2000@gmail.com>
2022-12-17 22:49:23 +0800

    Import helper func to setTheme based on media preference

--- src/Hooks/useDarkMode.tsx
+++ src/Hooks/useDarkMode.tsx
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getTheme, setTheme, getMediaPreference } from '../helpers/DarkMode';
 
 function useDarkMode() {
   const [isDarkMode, setMode] = useState(false);
 
+  useEffect(() => {
+    const userTheme = getTheme() || getMediaPreference();
+    const darkMode = userTheme === 'dark-mode';
+
+    if (darkMode) {
+      setMode(darkMode);
+      document.body.classList.add('dark-mode');
+    }
+  });
+
   function toggleDarkMode() {
+    if (isDarkMode) setTheme('light-mode');
+    else setTheme('dark-mode');
     setMode(!isDarkMode);
     document.body.classList.toggle('dark-mode');
   }
