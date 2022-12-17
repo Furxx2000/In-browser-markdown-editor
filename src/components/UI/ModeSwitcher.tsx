4d21d0be6458490d992b5722ae431ad36041eb36
furxx2000 <furxx2000@gmail.com>
2022-12-17 22:48:03 +0800

    Remove isDarkMode

--- src/components/UI/ModeSwitcher.tsx
+++ src/components/UI/ModeSwitcher.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import SvgIcon from './SvgIcon';
 import '../../scss/ModeSwitcher.scss';
 
@@ -13,10 +12,7 @@ function ModeSwitcher({ isDarkMode, toggleDarkMode }: Props) {
   return (
     <div className='mode-switcher flex'>
       <SvgIcon name='icon-dark-mode' color={isDarkMode ? 'white' : notActive} />
-      <div
-        className={`toggler bg-gray-1 ${isDarkMode ? 'dark-mode' : ''}`}
-        onClick={toggleDarkMode}
-      ></div>
+      <div className='toggler bg-gray-1' onClick={toggleDarkMode}></div>
       <SvgIcon
         name='icon-light-mode'
         color={isDarkMode ? notActive : 'white'}
