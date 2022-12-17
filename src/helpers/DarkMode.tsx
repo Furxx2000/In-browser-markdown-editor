51ae9c7595bb785d9a42fc600bbe6a1b800b9039
furxx2000 <furxx2000@gmail.com>
2022-12-17 22:48:56 +0800

    Create new helper func to set theme

---
+++ src/helpers/DarkMode.tsx
@@ -0,0 +1,19 @@
+import {} from './Regex';
+
+function getTheme() {
+  return localStorage.getItem('user-theme');
+}
+
+function setTheme(userTheme: string) {
+  localStorage.setItem('user-theme', userTheme);
+}
+
+function getMediaPreference() {
+  const hasDarkPreference = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
+  if (hasDarkPreference) return 'dark-mode';
+  else return 'light-mode';
+}
+
+export { getTheme, setTheme, getMediaPreference };
