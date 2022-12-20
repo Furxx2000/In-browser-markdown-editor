aa51c4bd34526e1ed7e99d084c0874da90d5c762
Danny Wen <danny_wen@trendmicro.com>
2022-12-20 11:18:09 +0800

    Add dark mode setting

--- src/components/UI/Toast.tsx
+++ src/components/UI/Toast.tsx
@@ -11,9 +11,7 @@ export default function Toast({ isToastOpen }: Props) {
       <SvgIcon name='icon-circle-check-solid' color='#f39765' />
       <div>
         <h5>Success</h5>
-        <p className='text-gray-2 fs-250'>
-          Your changes are saved successfully
-        </p>
+        <p className='fs-250'>Your changes are saved successfully</p>
       </div>
     </div>
   );
